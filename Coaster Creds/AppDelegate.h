7c176811dbc35cb8a0015d80fbcc072891496f48
Mike Fudge <mike@Mikes-MacBook-Air.local>
2015-03-16 18:04:15 +0000

    Initial Commit

---
+++ Coaster Creds/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Coaster Creds
+//
+//  Created by Mike Fudge on 16/03/2015.
+//  Copyright (c) 2015 Mike Fudge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Coaster Creds/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Coaster Creds
+//
+//  Created by Mike Fudge on 16/03/2015.
+//  Copyright (c) 2015 Mike Fudge. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
